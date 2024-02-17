2cdd3f6bf4a4f8385c8786f33d484132856a2cb8
worladev <confidenceworlasi@gmail.com>
2024-02-17 08:29:07 +0000

    git initialization

---
+++ finding-the-percentage.py
@@ -0,0 +1,31 @@
+'''
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a
+list of students. Print the average of the marks array for the student name provided, showing 2 places
+after the decimal.
+
+Example
+'alpha':[20,30,40]
+'beta':[30,50,70]
+query_name = 'beta'
+The query_name is 'beta'. beta's average score is (30 + 50 + 70)/3 = 50.0
+
+INPUT FORMAT: The first line contains the integer n, the number of students' records. The next n lines contain the
+names and marks obtained by a student, each value separated by a space. The final line contains
+query_name, the name of a student to query.
+
+CONSTRAINTS
+2 <= n <= 10
+0 <= marks[i] <= 100
+length of marks arrays = 3
+
+OUTPUT FORMAT: Print one line: The average of the marks obtained by the particular student correct to 2 decimal places.
+
+Sample Input
+3
+James 44 54 60
+Mary 40 55 70
+Mike 55 60 50
+Mary
+
+Output: 34.0 Explanation: (40 + 55 + 70)/3
+'''
